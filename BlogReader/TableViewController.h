a8384a1e6111016d4e2cb42d5d2f6a06f51a339d
Chloe Horgan <super.chloe@gmail.com>
2015-12-09 20:38:41 -0500

    Added TableViewController class

---
+++ BlogReader/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  BlogReader
+//
+//  Created by Chloe on 2015-12-09.
+//  Copyright © 2015 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *titles;
+
+@end
